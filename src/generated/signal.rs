22e9b80c03d8fce84994e6f6a25a62c831a304e2
li <li@goldragon.criome.net>
2026-09-21 12:58:43 -0600

    Generate Flow delivery permit signal contract v2

--- src/generated/signal.rs
+++ src/generated/signal.rs
@@ -10,6 +10,14 @@ pub type FlowType = String;
 #[rustfmt::skip]
 pub type Generation = i64;
 #[rustfmt::skip]
+pub type BindingGeneration = i64;
+#[rustfmt::skip]
+pub type LifecycleGeneration = i64;
+#[rustfmt::skip]
+pub type ProcessId = i64;
+#[rustfmt::skip]
+pub type ProcessStartTime = i64;
+#[rustfmt::skip]
 pub type EndpointPath = String;
 #[rustfmt::skip]
 pub type HerdrSessionName = String;
@@ -19,6 +27,26 @@ pub type HerdrAgentName = String;
 pub type HerdrPaneId = String;
 #[rustfmt::skip]
 pub type HerdrTerminalId = String;
+#[rustfmt::skip]
+pub type NativeThread = String;
+#[rustfmt::skip]
+pub type HarnessSession = String;
+#[rustfmt::skip]
+pub type RouteIdentity = String;
+#[rustfmt::skip]
+pub type EndpointIdentity = String;
+#[rustfmt::skip]
+pub type AttemptId = String;
+#[rustfmt::skip]
+pub type SourceEventIdentifier = String;
+#[rustfmt::skip]
+pub type DeliveryToken = String;
+#[rustfmt::skip]
+pub type TransitionId = String;
+#[rustfmt::skip]
+pub type RegistrationId = String;
+#[rustfmt::skip]
+pub type TransportReceiptId = String;
 #[rustfmt::skip]
 #[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
@@ -147,10 +175,158 @@ pub enum RecipientResolutionRejection {
 #[rustfmt::skip]
 #[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct DeliveryBinding {
+    pub native_thread: NativeThread,
+    pub harness_session: HarnessSession,
+    pub route_identity: RouteIdentity,
+    pub endpoint_identity: EndpointIdentity,
+    pub process_id: ProcessId,
+    pub process_start_time: ProcessStartTime,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct ExpectedOldBinding {
+    pub native_thread: NativeThread,
+    pub harness_session: HarnessSession,
+    pub route_identity: RouteIdentity,
+    pub endpoint_identity: EndpointIdentity,
+    pub process_id: ProcessId,
+    pub process_start_time: ProcessStartTime,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct ExpectedOldBindingGeneration {
+    pub binding_generation: BindingGeneration,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct DeliveryPermit {
+    pub attempt_id: AttemptId,
+    pub source_event_identifier: SourceEventIdentifier,
+    pub delivery_token: DeliveryToken,
+    pub binding_generation: BindingGeneration,
+    pub delivery_binding: DeliveryBinding,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct RefreshHeld_Data {
+    pub transition_id: TransitionId,
+}
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub enum AdmissionGate {
+    Open,
+    RefreshHeld(RefreshHeld_Data),
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct DeliveryState {
+    pub delivery_binding: DeliveryBinding,
+    pub binding_generation: BindingGeneration,
+    pub lifecycle_generation: LifecycleGeneration,
+    pub admission_gate: AdmissionGate,
+    pub delivery_permit: DeliveryPermit,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct AcquireDelivery {
+    pub flow_id: FlowId,
+    pub delivery_binding: DeliveryBinding,
+    pub binding_generation: BindingGeneration,
+    pub attempt_id: AttemptId,
+    pub source_event_identifier: SourceEventIdentifier,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct BeginRefresh {
+    pub flow_id: FlowId,
+    pub binding_generation: BindingGeneration,
+    pub transition_id: TransitionId,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct ReleaseConfirmed {
+    pub flow_id: FlowId,
+    pub attempt_id: AttemptId,
+    pub delivery_token: DeliveryToken,
+    pub delivery_binding: DeliveryBinding,
+    pub binding_generation: BindingGeneration,
+    pub transport_receipt_id: TransportReceiptId,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct ReadyReattach {
+    pub flow_id: FlowId,
+    pub transition_id: TransitionId,
+    pub expected_old_binding: ExpectedOldBinding,
+    pub expected_old_binding_generation: ExpectedOldBindingGeneration,
+    pub registration_id: RegistrationId,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct ReadDeliveryState {
+    pub flow_id: FlowId,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct RefreshHeld {
+    pub delivery_permit: DeliveryPermit,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct ReleaseReceipt {
+    pub attempt_id: AttemptId,
+    pub transport_receipt_id: TransportReceiptId,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub struct ReattachReceipt {
+    pub binding_generation: BindingGeneration,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
+pub enum DeliveryRejection {
+    UnknownFlow,
+    MissingState,
+    BindingUnavailable,
+    StaleBinding,
+    StaleGeneration,
+    RefreshHeld(RefreshHeld),
+    Busy,
+    AttemptConflict,
+    StalePermit,
+    TransitionConflict,
+    ActivePermit,
+    CorruptState,
+    GenerationOverflow,
+    StoreRefused,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "datom", derive(datom_codec::Datomizable, datom_codec::Composing))]
 pub enum Query {
     Start(StartRequest),
     Restart(RestartRequest),
     ResolveRecipient(RecipientResolutionRequest),
+    AcquireDelivery(AcquireDelivery),
+    BeginRefresh(BeginRefresh),
+    ReleaseConfirmed(ReleaseConfirmed),
+    ReadyReattach(ReadyReattach),
+    ReadDeliveryState(ReadDeliveryState),
 }
 #[rustfmt::skip]
 #[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
@@ -162,4 +338,18 @@ pub enum Response {
     StartRejected(StartRejection),
     RestartRejected(RestartRejection),
     RecipientResolutionRejected(RecipientResolutionRejection),
+    DeliveryGranted(DeliveryPermit),
+    DeliveryAlreadyGranted(DeliveryPermit),
+    DeliveryAcquireRejected(DeliveryRejection),
+    RefreshHeld(RefreshHeld),
+    RefreshAlreadyHeld(RefreshHeld),
+    RefreshRejected(DeliveryRejection),
+    DeliveryReleased(ReleaseReceipt),
+    DeliveryAlreadyReleased(ReleaseReceipt),
+    DeliveryReleaseRejected(DeliveryRejection),
+    DeliveryReattached(ReattachReceipt),
+    DeliveryAlreadyReattached(ReattachReceipt),
+    DeliveryReattachRejected(DeliveryRejection),
+    DeliveryStateRead(DeliveryState),
+    DeliveryStateRejected(DeliveryRejection),
 }
